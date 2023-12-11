9ce93888f2888c35ab905f9fed4a13a7d95c0bb8
Luc Grosheintz <luc.grosheintz@gmail.com>
2023-12-11 17:32:40 +0100

    Add license information. (#1)

--- tests/unit_tests.cpp
+++ tests/unit_tests.cpp
@@ -1,3 +1,12 @@
+/*************************************************************************
+ * Copyright (C) 2023 Blue Brain Project
+ *
+ * This file is part of 'libsonata-mpi', distributed under the terms
+ * of the GNU Lesser General Public License version 3.
+ *
+ * See top-level COPYING.LESSER and COPYING files for details.
+ *************************************************************************/
+
 #include <catch2/catch_session.hpp>
 
 #include <mpi.h>
