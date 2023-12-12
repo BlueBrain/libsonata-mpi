cf24893d7d4e5e2bb7c0b9922b0d908140eb5c6e
Luc Grosheintz <luc.grosheintz@gmail.com>
2023-12-12 08:19:50 +0100

    Setup automated testing.

---
+++ ci/python_test.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -euxo pipefail
+
+VENV="${PWD}/build/venv-python-test/"
+
+if [[ ! -d "$VENV" ]]; then
+    python3 -mvenv "$VENV"
+fi
+
+set +u  # ignore missing variables in activation script
+source "$BIN/activate"
+set -u
+
+$VENV/bin/pip -v install pytest
+$VENV/bin/pip -v install --force .
+
+# $VENV/bin/pytest
