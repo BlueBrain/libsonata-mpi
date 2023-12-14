291ea5b2f4bd48af7cc0f3620f4090b36bbbf761
Luc Grosheintz <luc.grosheintz@gmail.com>
2023-12-14 10:21:09 +0100

    Setup automated testing. (#4)

---
+++ ci/cpp_test.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# This file runs the C++ tests, as well as compiling the code with warnings on
+# so that errors should be caught quicker
+
+set -euxo pipefail
+
+BUILD_DIR=build/build-cpp-test
+rm -rf "$BUILD_DIR"
+mkdir -p "$BUILD_DIR"
+
+cmake -DCMAKE_BUILD_TYPE=Release                                             \
+      -DSONATA_MPI_FROM_SUBMODULES=ON                                        \
+      -B $BUILD_DIR                                                          \
+      .
+
+cmake --build "$BUILD_DIR" --parallel --verbose
+ctest --test-dir "$BUILD_DIR" --output-on-failure
---
+++ ci/python_test.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -euxo pipefail
+
+VENV="${PWD}/build/venv-python-test/"
+
+if [[ ! -d "$VENV" ]]; then
+    python3 -mvenv "$VENV"
+fi
+
+set +u  # ignore missing variables in activation script
+source "$VENV/bin/activate"
+set -u
+
+$VENV/bin/pip -v install pytest
+
+$VENV/bin/pip -v install extlib/libsonata
+$VENV/bin/pip -v install --force .
+
+$VENV/bin/pytest
